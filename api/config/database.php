51f4d121489793fc749be84fe71db639a350ed15
ArmandoMendez86 <desarrolloweb150305@gmail.com>
2025-10-28 23:20:49 -0600

    Prototipo producccion

--- api/config/database.php
+++ api/config/database.php
@@ -20,7 +20,7 @@ class Database {
 
         try {
             $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-            $this->conn->exec("set names utf8");
+            //$this->conn->exec("set names utf8");
             // Configura PDO para que lance excepciones en caso de error
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch(PDOException $exception) {
